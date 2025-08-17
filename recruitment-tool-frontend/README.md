0e928d13ae428463433bde05a1005fb0dfe76990
Joshua Mayhugh <123776276+jmayhugh1@users.noreply.github.com>
2025-08-16 23:20:37 -0500

    updating readme and frontend

--- recruitment-tool-frontend/README.md
+++ recruitment-tool-frontend/README.md
@@ -1,3 +1,25 @@
+
+##  On clonining the repo
+run npm install to install all the dependencies
+
+```bash
+## Versions 
+install nvim if you dont already have it
+```bash
+nvim --version
+```
+then run 
+```bash
+nvm install 21.7.3
+nvm use 21.7.3
+```
+
+then 
+```bash
+npm run dev
+```
+
+
 ## Formatting
 
 to run the formatter, use:
--- recruitment-tool-frontend/src/components/CandidateCard.tsx
+++ recruitment-tool-frontend/src/components/CandidateCard.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 interface CandidateCardProps {
   name: string;
   gradDate: number;
   imageUrl: string;
 }
-const CandidateCard = (props: CandidateCardProps) => {
-  return <div>CandidateCard</div>;
+
+const CandidateCard: React.FC<CandidateCardProps> = ({
+  name,
+  gradDate,
+  imageUrl,
+}) => {
+  return (
+    <Card sx={{ maxWidth: 345, margin: 'auto', boxShadow: 3 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={imageUrl}
+        alt={`${name}'s profile`}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Graduation Date: {gradDate}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary">
+          UpVote
+        </Button>
+        <Button size="small" color="secondary">
+          DownVote
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default CandidateCard;
