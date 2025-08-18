238caf11fcd808934ba903740743d7f1ce5b4f88
Joshua Mayhugh <123776276+jmayhugh1@users.noreply.github.com>
2025-08-17 19:13:14 -0500

    Creating voting system

--- recruitment-tool-frontend/src/components/CandidateCard.tsx
+++ recruitment-tool-frontend/src/components/CandidateCard.tsx
@@ -6,13 +6,45 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import type { Candidate } from '../types';
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const CandidateCard: React.FC<Candidate> = ({
+  id,
   name,
   grad_date,
   major,
   image_url,
 }) => {
+  const voteMap = {
+    up: 1,
+    down: -1,
+  };
+
+  const updateVotes = (voteType: 'up' | 'down') => {
+    const url = `${apiUrl}/vote`;
+    const params = {
+      id: id,
+      name: name,
+      increment: voteMap[voteType],
+    };
+    console.log('Updating vote for:', params);
+
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Vote updated:', data);
+      })
+      .catch((error) => {
+        console.error('Error updating vote:', error);
+      });
+  };
+
   return (
     <Card sx={{ maxWidth: 345, margin: 'auto', boxShadow: 3 }}>
       <CardMedia
@@ -33,10 +65,14 @@ const CandidateCard: React.FC<Candidate> = ({
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button size="small" color="primary" onClick={() => updateVotes('up')}>
           UpVote
         </Button>
-        <Button size="small" color="secondary">
+        <Button
+          size="small"
+          color="secondary"
+          onClick={() => updateVotes('down')}
+        >
           DownVote
         </Button>
       </CardActions>
